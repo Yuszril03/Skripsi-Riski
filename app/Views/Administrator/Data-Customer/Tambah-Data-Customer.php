bd762d3f39f0ca05ed8f950183554f6439f1a9b6
Yuszril03 <72553056+Yuszril03@users.noreply.github.com>
2022-06-25 22:51:54 +0700

    Tes

--- app/Views/Administrator/Data-Customer/Tambah-Data-Customer.php
+++ app/Views/Administrator/Data-Customer/Tambah-Data-Customer.php
@@ -238,6 +238,13 @@
         set,
         update
     } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-database.js";
+    import {
+        getStorage,
+        ref as refImage,
+        uploadBytesResumable,
+        getDownloadURL
+    } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-storage.js";
+
     // Your web app's Firebase configuration
     const firebaseConfig = {
         apiKey: "AIzaSyCBM7EKr0XU_nbfbX9vAliU9gPBTlgBhNw",
@@ -250,9 +257,52 @@
     };
     const app = initializeApp(firebaseConfig);
     const db = getDatabase();
+    const storage = getStorage();
 
     document.getElementById('submitData').addEventListener('click', function() {
         let idLogin = ['namaCust', 'emailCust', 'alamatCust', 'telpCust', 'tglCust'];
+
+        const fileupload = $('#uploadFilee').prop('files')[0];
+
+        const storageRef = refImage(storage, 'images-customer/' + fileupload.name);
+
+        // Upload the file and metadata
+        const uploadTask = uploadBytesResumable(storageRef, fileupload);
+
+        // Register three observers:
+        // 1. 'state_changed' observer, called any time the state changes
+        // 2. Error observer, called on failure
+        // 3. Completion observer, called on successful completion
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                // Observe state change events such as progress, pause, and resume
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            (error) => {
+                // Handle unsuccessful uploads
+            },
+            () => {
+                // Handle successful uploads on complete
+                // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log('File available at', downloadURL);
+                });
+            }
+        );
+
+
+
+        // console.log(fileupload)
     })
 </script>
 
@@ -307,22 +357,7 @@
         $('.image-upload-wrap').removeClass('image-dropping');
     });
 
-    mapboxgl.accessToken = 'pk.eyJ1Ijoic3VsdGFuMTIzIiwiYSI6ImNrZ3RmZHl3ejE5bTcyemxxc3BqeG5rdzcifQ.vOHwk-VTL573m2d6BfpLPw';
-    const coordinates = document.getElementById('coordinates');
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [117.1485239363954, -0.569178092470267],
-        zoom: 10
-    });
-
-    $("input").on("change", function() {
-        this.setAttribute(
-            "data-date",
-            moment(this.value, "YYYY-MM-DD")
-            .format(this.getAttribute("data-date-format"))
-        )
-    }).trigger("change")
+   
 
     function hanyaAngka(evt) {
         var charCode = (evt.which) ? evt.which : event.keyCode
