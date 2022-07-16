b6597950bdf6593a21a8f000109177b5afc9c7e1
Muhammadiki <34560594+Muhammadiki@users.noreply.github.com>
2022-07-17 00:57:37 +0800

    up

--- app/Views/Mitra/Rental/Data-Rental-Mobil.php
+++ app/Views/Mitra/Rental/Data-Rental-Mobil.php
@@ -205,12 +205,12 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">Data Kamar</h1>
+                            <h1 class="m-0">Data Mobil Rental</h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="<?= base_url() ?>/Beranda-Admin">Beranda</a></li>
-                                <li class="breadcrumb-item active">Data Kamar</li>
+                                <li class="breadcrumb-item active">Data Mobil Rental</li>
                             </ol>
                         </div><!-- /.col -->
                     </div><!-- /.row -->
